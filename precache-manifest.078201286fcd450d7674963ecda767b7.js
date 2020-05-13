abedf4004cc601a28e65e5e39fc63df9b39d7909
Oladiman <astonishingdami9@gmail.com>
2020-05-13 14:12:49 +0100

    Updates

---
+++ precache-manifest.078201286fcd450d7674963ecda767b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e7762f637569b7f4b2538e3258f9400",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "5e8de3d9e054e6f09a2e",
+    "url": "/robofriends/static/css/main.2cf63b60.chunk.css"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/robofriends/static/js/2.80c069d3.chunk.js"
+  },
+  {
+    "revision": "5e8de3d9e054e6f09a2e",
+    "url": "/robofriends/static/js/main.05629b3a.chunk.js"
+  },
+  {
+    "revision": "8bd552311f5d528a5605",
+    "url": "/robofriends/static/js/runtime~main.e3d5a5e7.js"
+  }
+]);
